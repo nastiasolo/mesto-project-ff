107df200fa72c9a1444391b2ac1f98a02cc98fcb
Ana S <ag.solodova@gmail.com>
2024-04-30 12:18:43 +0200

    first attempt

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,32 @@
 // @todo: Темплейт карточки
-
-// @todo: DOM узлы
+const placesList = document.querySelector(".places__list");
+const cardTemplate = document.querySelector("#card-template").content;
 
 // @todo: Функция создания карточки
 
+function createCard(item) {
+  // @todo: DOM узлы
+  const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
+
+  cardElement.querySelector(".card__image").src = item.link;
+  cardElement.querySelector(".card__image").alt = item.name;
+  cardElement.querySelector(".card__title").textContent = item.name;
+
+  const deleteButton = cardElement.querySelector(".card__delete-button");
+  deleteButton.addEventListener("click", () => {
+    deleteCard(deleteButton);
+  });
+
+  return cardElement;
+}
+
 // @todo: Функция удаления карточки
+function deleteCard(deleteButton) {
+  const listItem = deleteButton.closest(".places__item");
+  listItem.remove();
+}
 
 // @todo: Вывести карточки на страницу
+initialCards.forEach((element) => {
+  placesList.append(createCard(element));
+});
